65817caf2787a5ea0d64aec31527349104fb71e0
priyankayadav1998 <41065849+priyankayadav1998@users.noreply.github.com>
2025-09-02 01:25:37 +0530

    Add files via upload

---
+++ Assignment1.0.py
@@ -0,0 +1,6 @@
+# Question2
+c= input("enter the first name :")
+d= input("enter the second name :")
+e= c + " " + d
+f= "Hello, " + e + "!" + " welcome to program"
+print(f)
---
+++ Assignment1.py
@@ -0,0 +1,15 @@
+# Question1
+
+a= input ("Enter the first number: ")
+b= input ("Enter the second number: ")
+a= float(a)
+b= float(b)
+add= a+b
+print("addition is",add)
+sub= a-b
+print("subtraction is",sub)
+mul= a*b
+print("multiplication is",mul)
+div= a/b
+print("division is",div)
+
