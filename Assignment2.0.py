e72c1ee1beca004bb7a52954ebe3288eb9b422ce
priyankayadav1998 <41065849+priyankayadav1998@users.noreply.github.com>
2025-09-06 21:56:32 +0530

    Add files via upload

---
+++ Assignment2.0.py
@@ -0,0 +1,6 @@
+# check no is even or not
+n= int(input("enter a number "))
+if n%2==0:
+    print(n,"is an even number ")
+else:
+    print(n,"is an odd number")
---
+++ assignment2.1.py
@@ -0,0 +1,7 @@
+#1 sum of 1-50 integers using while
+sum=0
+counter=1
+while counter<=50:
+    sum=sum+counter
+    counter=counter+1
+print("sum of 1 to 50 int " , sum)
